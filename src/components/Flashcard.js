96fba9b91737380861207d52fb8329f01a2260f9
Jonher16 <80284536+Jonher16@users.noreply.github.com>
2022-03-03 21:46:55 +0100

    Added flashcards

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -5,24 +5,36 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "./Flashcard.css";
 import "../fonts/hkgokukaikk.ttf";
-import { IconButton } from "@material-ui/core";
-import {db} from "../firebase"
-import StarIcon from '@material-ui/icons/Star';
-import DeleteIcon from '@material-ui/icons/Delete';
-import CardActions from "@material-ui/core/CardActions"
+import { createTheme, IconButton } from "@material-ui/core";
+import { db } from "../firebase";
+import StarIcon from "@material-ui/icons/Star";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CardActions from "@material-ui/core/CardActions";
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 650,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+})
+const useStyles = makeStyles((theme) => ({
 
-const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    
+    minWidth: 200,
     maxHeight: "200vh",
     display: "flex",
     flexDirection: "row",
     marginBottom: "5vh",
     marginTop: "5vh",
-    padding: 5,
-    marginLeft:"1.5vw",
-    marginRight:"1.5vw"
+    padding: 4,
+    [theme.breakpoints.down("sm")]: {},
+    marginRight: "1.5vw",
   },
   desc: {
     fontSize: "0.75rem",
@@ -32,6 +44,9 @@ const useStyles = makeStyles({
     fontSize: "5rem",
     fontFamily: "hkgokukaikk",
     textAlign: "center",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "3rem",
+    },
   },
   hiragana: {
     fontSize: "2rem",
@@ -42,76 +57,128 @@ const useStyles = makeStyles({
     fontSize: "0.75rem",
     color: "grey",
     textAlign: "center",
-  
   },
   meaning: {
     marginTop: "1vh",
-    fontSize: "1rem"
+    fontSize: "1rem",
   },
   word__japanese: {
+      minWidth: "35vw",
+    [theme.breakpoints.down("sm")]: {
+      minWidth: "40vw",
+    },
+    [theme.breakpoints.down("xs")]: {
+      maxWidth: "50vw",
+    },
     borderRight: "1px solid black",
-    minWidth: "30vw"
+    
   },
-
   word__english: {
     minWidth: "50vw",
+    [theme.breakpoints.down("sm")]: {
+      minWidth: "30vw",
+    },
+    [theme.breakpoints.down("xs")]: {
+      minWidth: "10vw",
+    },
   },
-});
-const Flashcard = forwardRef(({id, username, timestamp, isFaved, kanji, hiragana, romaji, list, wordType, meaning}, ref) => {
-  const classes = useStyles();
-  const removeCard = () => {
-    db.collection("users/"+ username +"/"+ list).doc(id).delete()
-}
-const favCard = () => {
-  const favedCard = db.collection("cards").doc(id)
-  favedCard.get().then(doc=>favedCard.update({
-      isFaved: !doc.data().isFaved
+  buttons: {
+    position: "absolute",
+    right: "5vw",
+    [theme.breakpoints.down("xs")]: {
       
+    },
+  },
+}));
+const Flashcard = forwardRef(
+  (
+    {
+      id,
+      username,
+      timestamp,
+      isFaved,
+      kanji,
+      hiragana,
+      romaji,
+      list,
+      wordType,
+      meaning,
+    },
+    ref
+  ) => {
+    const classes = useStyles();
+    
+    const removeCard = () => {
+      db.collection("users/" + username + "/" + list)
+        .doc(id)
+        .delete();
+    };
+    const favCard = () => {
+      const favedCard = db.collection("users/" + username + "/" + list).doc(id);
+      favedCard
+        .get()
+        .then((doc) =>
+          favedCard.update({
+            isFaved: !doc.data().isFaved,
+          })
+        )
+        .catch((err) => console.log(err));
+    };
+    return (
+      <Card className={classes.root} variant="outlined" ref={ref}>
+        <CardContent className={classes.word__japanese}>
+          <Typography className={classes.title} variant="h1" component="h1">
+            {kanji}
+          </Typography>
+          <Typography className={classes.hiragana} variant="h2" component="h2">
+            {hiragana}
+          </Typography>
+          <Typography className={classes.romaji} variant="h3" component="h3">
+            {romaji}
+          </Typography>
+        </CardContent>
+        <CardContent className={classes.word__english}>
+          <Typography
+            className={classes.desc}
+            color="textSecondary"
+            gutterBottom
+          >
+            Added: {timestamp.seconds}
+          </Typography>
+          <Typography
+            className={classes.desc}
+            color="textSecondary"
+            gutterBottom
+          >
+            List: {list}
+          </Typography>
+          <Typography className={classes.desc} color="textSecondary">
+            Type: {wordType}
+          </Typography>
+          <Typography
+            className={classes.desc}
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            User: {username}
+          </Typography>
+          <Typography className={classes.meaning} variant="body2" component="p">
+            Meaning: {meaning}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <div className={classes.buttons}>
+            <IconButton onClick={removeCard}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton onClick={favCard} color={isFaved ? "primary" : ""}>
+              <StarIcon />
+            </IconButton>
+          </div>
+        </CardActions>
+      </Card>
+    );
   }
-  
-  ))
-  alert(isFaved)
-}
-  return (
-    <Card className={classes.root} variant="outlined" ref={ref}>
-      <CardContent className={classes.word__japanese}>
-        
-        <Typography className={classes.title} variant="h1" component="h1">
-          {kanji}
-        </Typography>
-        <Typography className={classes.hiragana} variant="h2" component="h2">
-          {hiragana}
-        </Typography>
-        <Typography className={classes.romaji} variant="h3" component="h3">
-         {romaji}
-        </Typography>
-      </CardContent>
-      <CardContent className={classes.word__english}>
-      <Typography className={classes.desc} color="textSecondary" gutterBottom>
-          Added: {timestamp.seconds}
-        </Typography>
-      <Typography className={classes.desc} color="textSecondary" gutterBottom>
-          List: {list}
-        </Typography>
-        <Typography className={classes.desc} color="textSecondary">
-          Type: {wordType}
-        </Typography>
-        <Typography className={classes.meaning} variant="body2" component="p">
-          Meaning: {meaning}
-        </Typography>
-        <Typography className={classes.meaning} variant="body2" component="p">
-          User: {username}
-        </Typography>
-      </CardContent>
-      <CardActions>
-      <IconButton onClick={removeCard}>
-          <DeleteIcon />
-      </IconButton>
-      <IconButton onClick={favCard} color={isFaved ? "primary" : ""}>
-          <StarIcon />
-      </IconButton>
-      </CardActions>
-    </Card>
-  );
-});
+);
 export default Flashcard;
--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -3,73 +3,132 @@ import { useEffect } from "react";
 import Flashcard from "./Flashcard";
 import { db } from "../firebase";
 import { useStateValue } from "../StateProvider";
-import { IconButton, MenuItem, TextField } from "@material-ui/core";
-import DeleteIcon from "@material-ui/icons/Delete"
+import { IconButton, MenuItem, TextField, Typography } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core/styles";
 const Flashcards = () => {
   const [cards, setCards] = useState([]);
   const [listList, setListList] = useState([]);
-  const [selectedList, setSelectedList] = useState({value: "List1"});
+  const [selectedList, setSelectedList] = useState({ value: null });
   const [{ user }, dispatch] = useStateValue();
-    useEffect(()=>{
-      db.collection("users/"+ user.displayName + "/userlists").onSnapshot(snapshot=>setListList(snapshot.docs.map(doc=>({
-        id: doc.id,
-        data: doc.data(),
-      }))))
-      db.collection("users/"+ user.displayName +"/"+ selectedList.value).orderBy("timestamp","desc").onSnapshot(snapshot=>setCards(snapshot.docs.map(doc=>({
-        id: doc.id,
-        data: doc.data(),
-      }))))
-    }, [selectedList])
 
-    const removeList = () => {
-      db.collection("users/"+ user.displayName + "/userlists").doc(listList.find(obj => {
-        return obj.data.value === selectedList.value
-      }).id).delete()
-    
-      alert(selectedList.value + " and cards in "+ selectedList.value + " were removed")
-      console.log(selectedList.value)
-      console.log(listList.find(obj => {
-        return obj.data.value === selectedList.value
-      }))
-    }
+  const classes = useStyles();
+  useEffect(() => {
+    db.collection("users/" + user.displayName + "/userlists").onSnapshot(
+      (snapshot) =>
+        setListList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+    );
+    db.collection("users/" + user.displayName + "/" + selectedList.value)
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setCards(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, [selectedList]);
+
+  const removeList = () => {
+    db.collection("users/" + user.displayName + "/userlists")
+      .doc(
+        listList.find((obj) => {
+          return obj.data.value === selectedList.value;
+        }).id
+      )
+      .delete();
+
+    alert(
+      selectedList.value +
+        " and cards in " +
+        selectedList.value +
+        " were removed"
+    );
+    console.log(selectedList.value);
+    console.log(
+      listList.find((obj) => {
+        return obj.data.value === selectedList.value;
+      })
+    );
+  };
   return (
-    <div>
+    <div className={classes.root}>
       <TextField
-          id="standard-select-word-type"
-          select
-          variant="outlined"
-          label="Select"
-          value={selectedList.value}
-          onChange={(e) => setSelectedList({ ...selectedList, value: e.target.value })}
-        >
-          {listList.map(({id, data: {value}}) => (
-            <MenuItem key={id} value={value}>
-              {value}
-            </MenuItem>
-          ))}
-        </TextField>
+        id="standard-select-word-type"
+        select
+        variant="outlined"
+        className={classes.selectList}
+        label="Select list"
+        value={selectedList.value}
+        onChange={(e) =>
+          setSelectedList({ ...selectedList, value: e.target.value })
+        }
+      >
+        {listList.map(({ id, data: { value } }) => (
+          <MenuItem key={id} value={value}>
+            {value}
+          </MenuItem>
+        ))}
+      </TextField>
+      {selectedList.value === null ? (
+        ""
+      ) : (
         <IconButton>
-          <DeleteIcon  onClick={removeList}/>
-      </IconButton>
+          <DeleteIcon onClick={removeList} />
+        </IconButton>
+      )}
 
-      {
-      cards.sort().map(({id, data: {username, timestamp, isFaved, kanji, hiragana, romaji, list, wordType, meaning}}) => (
-        <Flashcard
-          key={id}
-          id={id}
-          kanji={kanji}
-          hiragana={hiragana}
-          romaji={romaji}
-          list={list}
-          wordType={wordType}
-          meaning={meaning}
-          isFaved={isFaved}
-          timestamp={timestamp}
-          username={username}
-        />
-      ))}
+      {cards
+        .sort()
+        .map(
+          ({
+            id,
+            data: {
+              username,
+              timestamp,
+              isFaved,
+              kanji,
+              hiragana,
+              romaji,
+              list,
+              wordType,
+              meaning,
+            },
+          }) => (
+            <Flashcard
+              key={id}
+              id={id}
+              kanji={kanji}
+              hiragana={hiragana}
+              romaji={romaji}
+              list={list}
+              wordType={wordType}
+              meaning={meaning}
+              isFaved={isFaved}
+              timestamp={timestamp}
+              username={username}
+            />
+          )
+        )}
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginLeft: "2vw",
+    marginTop: "5vh",
+    
+  },
+  selectList: {
+    minWidth: "12vw",
+  },
+}));
 
 export default Flashcards;
